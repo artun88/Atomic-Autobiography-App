fb01869ee8ae6a8870739c6482805562cffe8224
Artun Kircali <artun88@users.noreply.github.com>
2014-04-29 05:16:02 -0400

    Update Applicant.cs

--- Applicant.cs
+++ Applicant.cs
@@ -1,13 +1,15 @@
-/*Artun Kircali - 4/29/14
-* Applicant class:
-* Contains information relating to the user entering responses into
-*   the form's text boxes.
-* Properties should include: user ID, save/load/submit file directory
-*   location, array of strings to hold info of form questions written
-*   on windows form labels, array of strings to hold user input received 
-*   from rich text boxes, and a value of bool type that detects if the
-*   user had made a change has been made to the form.
-*/
+/* Atomic Autobiography App: by Artun Kircali, 4/29/14
+ * ---------------------------------------------------
+ * Applicant Class:
+ * Contains information relating to the user entering responses into
+ *   the form's text boxes.
+ * Properties should include: user ID, save/load/submit file directory
+ *   location, array of strings to hold info of form questions written
+ *   on windows form labels, array of strings to hold user input received 
+ *   from rich text boxes, and a value of bool type that detects if the
+ *   user had made a change has been made to the form.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -83,14 +85,16 @@ namespace Atomic_Object_Job_Application
             }
         }
 
-        //Records user's responses to form questions
+        //Records & keeps track of user's responses.
         public void addResponse(int i, string str)
         {
             answers[i] = str;
         }
 
-        //Removes all instances of new lines so file
-        //can be saved properly
+        //Removes all instances of new lines so that
+        //  answers[] can be properly refilled during loadFormData().
+        //If new lines aren't removed, responses can't be saved
+        //  to correct indices of answers[].
         public void removeNewLines(ref string[] answers)
         {
             for (int i = 0; i < answers.Length; i++)
@@ -100,8 +104,29 @@ namespace Atomic_Object_Job_Application
             }
         }
 
-        //Recursively edits answers for readability
-        //by extending each line only 70 characters from left
+        //Removes extraneous whitespace between words in user's
+        //  responses to maintain neat consistent formatting in submission.
+        public string trimSpaceBtwStrings(string str)
+        {
+            string[] words = str.Split(new string[] {" "},
+                StringSplitOptions.None);
+            string trimmedStr = null;
+            
+            //Cycle through entire resposne, adding each word to
+            //  a new string with only one space between each word.
+            for(int i = 0; i < words.Length; i++)
+            {
+                string result = words[i].Trim();
+
+                if(!string.IsNullOrWhiteSpace(result))
+                    trimmedStr += result + " ";
+            }
+
+            return trimmedStr.Trim();
+        }
+
+        //Recursively edits answers for readability in submission
+        //  by extending each line only 70 characters from left.
         public string putInParagraphs(string str)
         {
             int count = 70;
@@ -123,18 +148,22 @@ namespace Atomic_Object_Job_Application
             return first;
         }
 
-        //Formats submitted document by adding answer
-        //array to array of questions. For readability.
+        //Fills out autobiography document.
+        //During submission, responses from answers[] are added to
+        //  set questions in appQs[] so they can be written to file.
+        //Basically returns appQs[] + answers[].
         private string[] mergeQsAs()
         {
             string[] filledApp = new string[appQs.Length];
             int i = 0;
 
+            //Page 1 questions
             for (i = 0; i < 7; i++)
             {
                 filledApp[i] = appQs[i] + answers[i];
             }
 
+            //Page 2 question
             if (answers[7] == "Yes")
             {
                 filledApp[7] = appQs[7];
@@ -143,7 +172,7 @@ namespace Atomic_Object_Job_Application
             else
                 filledApp[7] = appQs[7] + answers[7];
 
-
+            //Page 2 - 10 questions
             for (i = 9; i < appQs.Length; i++)
             {
                 filledApp[i] = appQs[i] +
@@ -153,28 +182,25 @@ namespace Atomic_Object_Job_Application
             return filledApp;
         }
 
-        private static FileAttributes putAttributes(FileAttributes attribute, 
-                                                    FileAttributes attRemove)
-        {
-            return attribute & ~attRemove;
-        }
-
-        //Creates data files containing non-submitted form data
-        //for user to return to and continue editing before submission
+        //Saves user's current responses to a read-only 
+        //  file in hidden folder.
+        //Returns false: If set directory is missing or incorrect.
+        //Returns true: If file is opened and written successfully.
         public bool saveFormData()
         {
             propChange = false;
             string pathFile = pathDir + @"Saved Form Data\" + userID + ".txt";
 
-            UTF8Encoding enc = new UTF8Encoding();
-
-            //Allows file to be saved again without error
+            //Changes file's permissions so it can be read
             if (File.Exists(pathFile))
+            {
                 File.SetAttributes(pathFile,
                     File.GetAttributes(pathFile) & ~FileAttributes.ReadOnly);
+            }
 
+            //Saves to a UTF8 encoded text file
             removeNewLines(ref answers);
-
+            UTF8Encoding enc = new UTF8Encoding();
             try
             {
                 File.WriteAllLines(pathFile, answers, enc);
@@ -189,11 +215,15 @@ namespace Atomic_Object_Job_Application
             }
         }
 
-        //Retrieves contents of autobiography from saved data file
+        //Loads user's responses from a previous session.
+        //Returns false: If a directory is missing, a bad username was
+        //  given, or the file couldn't be opened (i.e. missing, corrupt).
+        //Returns true: If file successfully opened & read.
         public bool loadFormData()
         {
             string pathFile = pathDir + @"Saved Form Data\";
 
+            //Checks if intended target directory is set
             if (!Directory.Exists(pathFile))
             {
                 MessageBox.Show("Missing directory.", "Error", 
@@ -201,9 +231,9 @@ namespace Atomic_Object_Job_Application
                 return false;
             }
 
+            //Loads from a UTF8 encoded text file
             pathFile += userID + ".txt";
             UTF8Encoding enc = new UTF8Encoding();
-
             try
             {
                 File.SetAttributes(pathFile, 
@@ -220,8 +250,12 @@ namespace Atomic_Object_Job_Application
             return true;
         }
 
-        //Creates a formatted text file displaying applicant's
-        //autobiography information received from this form
+        //Saves a 'filled out' version of the form to a local directory
+        //  as a neatly organized, formatted text document.
+        //Returns false: if response formatting process fails, a file of
+        //  the same name exists (user submits with same email twice),
+        //  or the file could not be opened.
+        //Returns true: If file saves successfully to specified directory.
         public bool submitApplication()
         {
             //Formats answer file for ease of reading
@@ -232,27 +266,30 @@ namespace Atomic_Object_Job_Application
                 {
                     if (!string.IsNullOrWhiteSpace(answers[i]))
                     {
+                        answers[i] = trimSpaceBtwStrings(answers[i]);
                         answers[i] = putInParagraphs(answers[i]);
                         answers[i].TrimEnd();
                     }
                 }
             }
+            //answers[] has gone out of bounds during formatting process
             catch
             {
-                MessageBox.Show("Sorry, there was an issue submitting your " +
-                    "autobiography.", "Error", MessageBoxButtons.OK,
+                MessageBox.Show("Sorry, there was an issue submitting " +
+                    "your autobiography.", "Error", MessageBoxButtons.OK,
                     MessageBoxIcon.Exclamation);
                 return false;
             }
-            
-            string[] filledApp = mergeQsAs();
-            string[] appName = answers[0].Split(' ');
 
-            //id's user submission using their e-mail
+            string[] filledApp = mergeQsAs(); //Fills out form
+            
+            //Name's file using user's name and e-mail 
+            string[] appName = answers[0].Split(new string[] {" "},
+                StringSplitOptions.None);
             string pathFile = pathDir + @"Submits\" + appName.First() +
                 "_" + appName.Last() + "_" + userID + ".txt";
-
-            //Checks if user already submitted autobiography
+                 
+            //Checks if user already submitted using this e-mail address
             if (File.Exists(pathFile))
             {
                 MessageBox.Show("This user has already submitted an autobiography",
