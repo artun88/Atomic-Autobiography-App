50a0ece1e21fb47be6358b8b1779db395e0a38ff
Artun Kircali <artun88@users.noreply.github.com>
2014-04-29 05:17:13 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+/* Atomic Autobiography App: by Artun Kircali, 4/29/14
+ * ---------------------------------------------------
+ * Contains Main()
+ * Runs my application.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atomic_Object_Job_Application
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new page1());
+        }
+    } 
+}
